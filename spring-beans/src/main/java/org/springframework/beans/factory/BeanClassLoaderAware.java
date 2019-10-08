c96972ab1055bd3d463fe8b1f424a9c46f24bfa8
blackCat-09 <wodrjing@163.com>
2019-10-08 23:52:16 +0800

    aware

--- spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
@@ -50,6 +50,11 @@ public interface BeanClassLoaderAware extends Aware {
 	 * the {@code ClassLoader} obtained via
 	 * {@link org.springframework.util.ClassUtils#getDefaultClassLoader()}
 	 */
+	/**
+	 * 将 BeanClassLoader 提供给 bean 实例回调
+	 * 在 bean 属性填充之后、初始化回调之前回调，
+	 * 例如InitializingBean的InitializingBean.afterPropertiesSet（）方法或自定义init方法
+	 */
 	void setBeanClassLoader(ClassLoader classLoader);
 
 }
